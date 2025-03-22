8586ff93bfa45a9f324f44dec74a6a035aa10c01
Letícia <111805836+letiicia1@users.noreply.github.com>
2025-03-22 17:59:44 -0300

    Update Principal.java

--- Project/Principal.java
+++ Project/Principal.java
@@ -13,11 +13,11 @@ public class Principal {
             int opcao;
             do {
 
-                System.out.println("\n\nPUCFlix 1.0");
+                System.out.println("PUCFlix 1.0");
                 System.out.println( "-----------");
                 System.out.println("> Início");
-                System.out.println("\n1 - Series");
-                System.out.println("\n2 - Episodios");
+                System.out.println("1 - Series");
+                System.out.println("2 - Episodios");
                 System.out.println("0 - Sair");
                 
                 System.out.print("\nOpção: ");
